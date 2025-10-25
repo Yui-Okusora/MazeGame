5f27fe63b3d0ef8bb42ceac2f0fd7a2772eacde4
Yui-Okusora <dqdung3112@gmail.com>
2025-10-25 20:43:16 +0700

    Add project files.

---
+++ src/MazeGame.cpp
@@ -0,0 +1,12 @@
+﻿// MazeGame.cpp : Defines the entry point for the application.
+//
+
+#include "MazeGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
